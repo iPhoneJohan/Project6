c1b768902a8f4efeabaeca9b3e313b7f1ae373a5
????? <hejsan1993@live.se>
2019-05-08 12:55:24 +0200

    added methods

--- OurHashTable.java
+++ OurHashTable.java
@@ -2,7 +2,21 @@ public class OurHashTable<AnyType> {
 
 	private AnyType[] theArray;
 
+	
+	
+	
+	public void add() {
+		
+	}
+	public void find(AnyType s) {
+		
+	}
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
-
+		
 	}
 }
